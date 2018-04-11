dc5708cd72eddbda7958192f04ea69311646831e
bouzuya <m@bouzuya.net>
2018-04-11 23:11:36 +0900

    add addStampCard

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,14 @@
 module Lib
-    ( someFunc
+    ( addStampCard
+    , someFunc
     , stampRally1
-    ) where
+    , stampRally1'
+    )
+where
+
+import           Data.List      (last)
+import           Data.Maybe     (Maybe (Just, Nothing), maybe)
+import           Data.Semigroup ((<>))
 
 newtype SpotId = SpotId Integer deriving Show
 newtype Spot = Spot SpotId deriving Show
@@ -12,10 +19,27 @@ data StampCard = StampCard StampCardId [Stamp] deriving Show
 newtype StampRallyId = StampRallyId Integer deriving Show
 data StampRally = StampRally StampRallyId [Spot] [StampCard] [Stamp] deriving Show
 
+last' :: [a] -> Maybe a
+last' [] = Nothing
+last' xs = Just $ last xs
+
 spots1 = [Spot (SpotId 1), Spot (SpotId 2), Spot (SpotId 3)]
 
 stampRally1 :: StampRally
 stampRally1 = StampRally (StampRallyId 1) spots1 [] []
+
+addStampCard :: StampRally -> (StampCard, StampRally)
+addStampCard (StampRally id spots cards stamps) =
+    (newCard, StampRally id spots newCards stamps)
+  where
+    lastCard  = last' cards
+    newCardId = StampCardId
+        $ maybe 1 (\(StampCard (StampCardId id) _) -> id + 1) lastCard
+    newCard  = StampCard newCardId []
+    newCards = cards <> [newCard]
+
+stampRally1' :: StampRally
+stampRally1' = snd $ addStampCard $ snd $ addStampCard stampRally1
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
