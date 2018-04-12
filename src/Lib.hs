3dbbf695bd11033dff82814f5aad45043e27c55f
bouzuya <m@bouzuya.net>
2018-04-12 14:35:06 +0900

    add getSpot(s)

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,14 +6,18 @@ module Lib
     , stampRally1'
     )
 where
+
+import           Data.Bool      ((&&))
+import           Data.Foldable  (length)
 import           Data.Functor   ((<$>))
-import           Data.List      (last)
+import           Data.Int       (Int)
+import           Data.List      (last, (!!))
 import           Data.Maybe     (Maybe (Just, Nothing), maybe)
 import           Data.Semigroup ((<>))
 import           Data.Tuple     (snd)
 import           Data.UUID      (UUID)
 import           Data.UUID.V4   (nextRandom)
-import           Prelude        (Integer, Show, ($), (+))
+import           Prelude        (Integer, Show, ($), (+), (<), (<=))
 import           System.IO      (IO, print, putStrLn)
 
 newtype SpotId = SpotId Integer deriving Show
@@ -48,6 +52,14 @@ addStampCard (StampRally id spots cards stamps) =
 stampRally1' :: StampRally
 stampRally1' = snd $ addStampCard $ snd $ addStampCard stampRally1
 
+getSpots :: StampRally -> [Spot]
+getSpots (StampRally _ spots _ _) = spots
+
+getSpot :: StampRally -> Int -> Maybe Spot
+getSpot r index = case getSpots r of
+    [] -> Nothing
+    xs -> if 0 <= index && index < length xs then Just (xs !! index) else Nothing
+
 genStampId :: IO StampId
 genStampId = StampId <$> nextRandom
 
@@ -55,4 +67,6 @@ someFunc :: IO ()
 someFunc = do
     stampId <- genStampId
     print stampId
+    let spot = getSpot stampRally1' 4
+    print spot
     putStrLn "someFunc"
